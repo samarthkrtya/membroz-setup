35e1ed26fd27427977ad72933aaf7ad8e7cc6bb7
hirenmistry17 <hirenmistry.krtya@gmail.com>
2022-07-05 16:35:46 +0530

    fixed

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -26,10 +26,11 @@ export class Configuration {
     }
     
     this.baseUrl = location.origin + '/#';
+    
 
       if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
         // this.Server = 'http://localhost:3001/';
-        this.Server = 'https://app.membroz.com/';
+        this.Server = 'https://surgefitnesslifestyle.membroz.com';
         this.actionUrl = this.Server + 'api/';
       } else {
         this.Server = 'https://' + location.hostname + '/';
